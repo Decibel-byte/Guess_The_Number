889c8a4b5c9f74702bb76d904e1f031ffda5da67
Decibel-byte <58370459+Decibel-byte@users.noreply.github.com>
2020-05-28 23:18:29 +0530

    Update guess_the_number.py

--- guess_the_number.py
+++ guess_the_number.py
@@ -8,14 +8,14 @@ for i in range(1,10):
         count = count - 1
         print('Number of guesses left : ',count)
     elif (15<=num1<=17):
-        print('Increase your number by a little difference')
+        print('Increase your number by a small difference')
         count = count - 1
         print('Number of guesses left : ',count)
     elif (num1 == n):
         print('Congratulations! You have guess the correct number.')
         break
     elif (num1 >n and num1<21):
-        print('Decrease your number by a little difference')
+        print('Decrease your number by a small difference')
         count = count - 1
         print('Number of guesses left : ',count)
 while (count == 0):
